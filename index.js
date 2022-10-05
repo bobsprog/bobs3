22029ec1312d15c2b7b778d734638790759e9ba3
bobsprog <your@email.com>
2022-10-05 19:46:31 +0200

    hhh

---
+++ index.js
@@ -0,0 +1,21 @@
+let countEl=document.getElementById("count-el");
+let count=0;
+
+
+function increment(){
+    count=count+1;
+    countEl.innerText=count;
+    console.log(count);
+}
+
+
+function save(){
+    console.log(count);
+}
+
+
+
+
+
+
+
